43e6b46fbcd4a0a6cdc0638ec3b0830e1a4dcf43
gbLee <kblee@ispark.kr>
2026-03-03 14:58:14 +0900

    git rm 배치파일일

--- teamagent-debugger-tomcat.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-set JAVA_HOME=C:\Program Files\Java\jdk-11
-set PATH=%JAVA_HOME%\bin;%PATH%
-set JPDA_ADDRESS=8001
-set JPDA_TRANSPORT=dt_socket
-cd /d "D:\utils\apache\apache-tomcat-8.5.68-windows-x64\apache-tomcat-8.5.68-teamagent\bin"
-@REM call catalina.bat jpda run
-call catalina-jrebel.bat jpda run
