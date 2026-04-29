0b84d4fecb6b112034a2a4fb19030a3f64edb608
jwyoun <jwyoun@ispark.kr>
2026-04-29 11:04:39 +0900

    [ 조직관리 > 서비스 구현 ] 1. 조직 등록 및 수정 기능 추가 2. 사용자 목록 조회 기능 추가 3. 조직 정렬순서 수정 기능 추가 4. 조직 삭제 기능 추가 5. SQL 쿼리 및 매퍼 추가/수정 6. 프로필 이미지 URL 생성 로직 추가 7. ORG_ID 전용 generateTableKey 로직 생성

--- src/main/java/kr/teamagent/orgmanage/service/OrgManageVO.java
+++ src/main/java/kr/teamagent/orgmanage/service/OrgManageVO.java
@@ -1,15 +1,22 @@
 package kr.teamagent.orgmanage.service;
 
+import java.util.List;
+
 import kr.teamagent.common.CommonVO;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 조직/사용자 화면용 VO. {@code selectOrgList} 는 조직 컬럼만,
+ * {@code selectUserList} 는 사용자 컬럼을 채운다.
+ */
 @Getter
 @Setter
 public class OrgManageVO extends CommonVO {
 
     private static final long serialVersionUID = 1L;
 
+    /** TB_ORG.ORG_ID */
     private String orgId;
     private String orgNm;
     private String parentOrgId;
@@ -18,4 +25,22 @@ public class OrgManageVO extends CommonVO {
     private String useYn;
     private String createdDt;
     private String updatedDt;
+    private String userId;
+    private String userNm;
+    private String email;
+    private String phone;
+    private String profileImgPath;
+    private String profileImgUrl;
+    private String acctStatusCd;
+    private String acctStatusDesc;
+
+    /** 동일 부모 내 정렬 일괄 반영용 (orgManage.updateSiblingSortOrderBatch) */
+    private List<OrgSortOrderItem> items;
+
+    @Getter
+    @Setter
+    public static class OrgSortOrderItem {
+        private String orgId;
+        private String sortOrder;
+    }
 }
--- src/main/java/kr/teamagent/orgmanage/service/impl/OrgManageDAO.java
+++ src/main/java/kr/teamagent/orgmanage/service/impl/OrgManageDAO.java
@@ -1,5 +1,6 @@
 package kr.teamagent.orgmanage.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Repository;
@@ -19,5 +20,97 @@ public class OrgManageDAO extends EgovComAbstractDAO {
     public List<OrgManageVO> selectOrgList(OrgManageVO searchVO) throws Exception {
         return selectList("orgManage.selectOrgList", searchVO);
     }
-    
+
+    /**
+     * 동일 부모 조직 목록 조회 (최상위는 parentOrgId null)
+     * @param parentOrgId
+     * @return list
+     * @throws Exception
+     */
+    public List<OrgManageVO> selectParentOrgId(String parentOrgId) throws Exception {
+        return selectList("orgManage.selectParentOrgId", parentOrgId);
+    }
+
+    /**
+     * 사용자 목록 조회
+     * @param orgId
+     * @return list
+     * @throws Exception
+     */
+    public List<OrgManageVO> selectUserList(String orgId) throws Exception {
+        HashMap<String, Object> paramMap = new HashMap<>();
+        paramMap.put("orgId", orgId);
+        return selectList("orgManage.selectUserList", paramMap);
+    }
+
+    /**
+     * 조직 등록
+     * @param orgManageVO
+     * @return
+     * @throws Exception
+     */
+    public int insertOrg(OrgManageVO orgManageVO) throws Exception {
+        return insert("orgManage.insertOrg", orgManageVO);
+    }
+
+    /**
+     * 동일 부모 조직 내 최대 정렬순서 조회
+     * @param parentOrgId
+     * @return max sort order
+     * @throws Exception
+     */
+    public int selectMaxSortOrderByParentOrgId(String parentOrgId) throws Exception {
+        return (int) selectOne("orgManage.selectMaxSortOrderByParentOrgId", parentOrgId);
+    }
+
+    /**
+     * 최상위 조직(ORG_LEVEL=1) 최대 정렬순서 조회
+     * @return max sort order
+     * @throws Exception
+     */
+    public int selectMaxSortOrderByTopLevel() throws Exception {
+        return (int) selectOne("orgManage.selectMaxSortOrderByTopLevel", null);
+    }
+
+    /**
+     * 조직 수정
+     * @param orgManageVO
+     * @return
+     * @throws Exception
+     */
+    public int updateOrg(OrgManageVO orgManageVO) throws Exception {
+        return update("orgManage.updateOrg", orgManageVO);
+    }
+
+    /**
+     * 동일 부모 조직 정렬순서 일괄 수정
+     * @param orgManageVO
+     * @return
+     * @throws Exception
+     */
+    public int updateSiblingSortOrderBatch(OrgManageVO orgManageVO) throws Exception {
+        return update("orgManage.updateSiblingSortOrderBatch", orgManageVO);
+    }
+
+    /**
+     * 조직 단건 조회
+     * @param orgId
+     * @return vo
+     * @throws Exception
+     */
+    public OrgManageVO selectOrgByOrgId(String orgId) throws Exception {
+        return (OrgManageVO) selectOne("orgManage.selectOrgByOrgId", orgId);
+    }
+
+    /**
+     * 조직 삭제
+     * @param orgManageVO
+     * @return
+     * @throws Exception
+     */
+    public int deleteOrg(OrgManageVO orgManageVO) throws Exception {
+        return update("orgManage.deleteOrg", orgManageVO);
+    }
 }
+
+
